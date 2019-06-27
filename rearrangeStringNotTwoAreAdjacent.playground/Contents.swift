fa19d24c800698619de865fbbbea74d4df1b5d0f
Priya <priya.mobdev@gmail.com>
2019-06-27 11:35:21 +0530

    Panagram and Longest prefix done

---
+++ rearrangeStringNotTwoAreAdjacent.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Helloplayground"
+
+func reArrangeCharacters() {
+    var tempArray = [Character]()
+    var characters = Array(str)
+    for (index,value) in (0..<characters.count - 1).enumerated() {
+        if characters[index] == characters[index + 1] {
+            tempArray.append(characters[index + 1])
+        }
+    }
+    for j in 0..<tempArray.count {
+        characters[(characters.count) - (tempArray.count)] = tempArray[j]
+    }
+    print (characters)
+}
+reArrangeCharacters()
+
